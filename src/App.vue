b79d925076d91c1aa329b979753d3c989c93372f
cristianocsl <cslcristiano@gmail.com>
2022-05-24 01:41:49 -0300

    feat: adiciona componente com a tabela

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view />
+  <TableValueVue />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import TableValueVue from './components/TableValue.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    TableValueVue,
+  },
 });
 </script>
